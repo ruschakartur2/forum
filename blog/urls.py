5c72bc90173491a659a58c78451dd3af688ebf4f
ruschakartur2 <ruschakartur@gmail.com>
2020-12-10 23:09:51 +0200

    Created CRUD for topics. Added bootstrap and some styles

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TopicListView, TopicUpdateView, TopicDetailView, TopicDeleteView
+from .views import TopicListView, TopicUpdateView, TopicDetailView, TopicDeleteView, TopicCreateView
 
 urlpatterns = [
-    path('',TopicListView.as_view(), name='topic_list'),
-    path('<int:pk>/edit',TopicUpdateView.as_view(),name='topic_edit'),
-    path('<int:pk>/',TopicDetailView.as_view(),name='topic_detail'),
-    path('<int:pk>/delete',TopicDeleteView.as_view(),name='topic_delete')
+    path('', TopicListView.as_view(), name='topic_list'),
+    path('<int:pk>/edit/', TopicUpdateView.as_view(), name='topic_edit'),
+    path('<int:pk>/', TopicDetailView.as_view(), name='topic_detail'),
+    path('<int:pk>/delete/', TopicDeleteView.as_view(), name='topic_delete'),
+    path('new/', TopicCreateView.as_view(), name='topic_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,20 +8,37 @@ from django.views.generic import TemplateView, ListView, CreateView, UpdateView,
 from .models import Topic
 
 
-class TopicListView(ListView):
+class TopicCreateView(LoginRequiredMixin, CreateView):
+    model = Topic
+    template_name = 'topic/topic_new.html'
+    fields = ('title', 'body')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class TopicListView(LoginRequiredMixin, ListView):
     model = Topic
     template_name = 'topic/topic_list.html'
+    login_url = 'login'
 
-class TopicDetailView(DetailView):
-    model = Topic
-    template_name = 'topic_detail.html'
 
-class TopicUpdateView(UpdateView):
+class TopicDetailView(LoginRequiredMixin, DetailView):
     model = Topic
-    fields = ('title','body')
-    template_name = 'topic_edit.html'
+    template_name = 'topic/topic_detail.html'
+    login_url = 'login'
 
-class TopicDeleteView(DeleteView):
+
+class TopicUpdateView(LoginRequiredMixin, UpdateView):
     model = Topic
-    template_name = 'topic_delete.html'
-    success_url = reverse_lazy('article_list')
+    fields = ('title', 'body')
+    template_name = 'topic/topic_edit.html'
+    login_url = 'login'
+
+
+class TopicDeleteView(LoginRequiredMixin, DeleteView):
+    model = Topic
+    template_name = 'topic/topic_delete.html'
+    success_url = reverse_lazy('topic_list')
+    login_url = 'login'
