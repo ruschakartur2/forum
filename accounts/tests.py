195348ef300d42c5802f385dd1aa0458ac4e0096
ruschakartur2 <ruschakartur@gmail.com>
2020-12-21 10:54:03 +0200

    Created test to oauth2 authorization

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,31 +2,54 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 # Create your tests here.
-from django.urls import reverse
+
+from oauth2_provider.settings import oauth2_settings
+from oauth2_provider.models import get_access_token_model,get_application_model
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+Application = get_application_model()
+AccessToken = get_access_token_model()
+UserModel = get_user_model()
 
 
-class SignUpTests(TestCase):
-    username = 'testuser'
-    email = 'testuser@gmail.com'
+class Test_mytest(APITestCase):
 
-    def test_signup_page_status_code(self):
-        response = self.client.get('/accounts/signup/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
 
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
+        oauth2_settings._SCOPES = ["read", "write", "scope1", "scope2", "resource1"]
 
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'registration/signup.html')
+        self.test_user = UserModel.objects.create_user("test_user", "test@example.com", "123456")
 
-    def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(
-            self.username, self.email
-        )
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+        self.application = Application.objects.create(
+                                                name="Test Application",
+                                                redirect_uris="http://localhost http://example.com http://example.org",
+                                                user=self.test_user,
+                                                client_type=Application.CLIENT_CONFIDENTIAL,
+                                                authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+                                            )
+
+        self.access_token = AccessToken.objects.create(
+                                                    user=self.test_user,
+                                                    scope="read write",
+                                                    expires=timezone.now() + timezone.timedelta(seconds=300),
+                                                    token="secret-access-token-key",
+                                                    application=self.application
+                                                )
+        # read or write as per your choice
+        self.access_token.scope = "read"
+        self.access_token.save()
+
+        # correct token and correct scope
+        self.auth =  "Bearer {0}".format(self.access_token.token)
+
+    def test_success_response(self):
+
+        url = reverse('login',)
+
+        # Obtaining the POST response for the input data
+        response = self.client.get(url, HTTP_AUTHORIZATION=self.auth)
 
--- blog/tests.py
+++ blog/tests.py
@@ -4,19 +4,3 @@ from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
 
 
-class HomePageTest(SimpleTestCase):
-
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'topic_list.html')
-
-
